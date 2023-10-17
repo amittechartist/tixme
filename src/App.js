9023bee74863bc2941a49ffe40c15e750c01bcd4
unknown <amittechartist@gmail.com>
2023-10-17 19:12:18 +0530

    17-10

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './pages/organizer/Dashboard';
 import EventType from './pages/organizer/Event/Type';
 import EventDetails from './pages/organizer/Event/EventDetails';
 import EventPhotos from './pages/organizer/Event/Photo';
-import EventBasicInfo from './pages/organizer/Event/Photo';
+import EventBasicInfo from './pages/organizer/Event/BasicInfo';
 
 import OrganizerLayout from './layout/organizer/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -15,7 +15,7 @@ function App() {
   return (
       <BrowserRouter>
       <Routes>
-        <Route path={organizer_url+'event/dashboard'} element={<OrganizerLayout> <Dashboard title={'dashboard'} /> </OrganizerLayout>} />
+        <Route path={'/'} element={<OrganizerLayout> <Dashboard title={'dashboard'} /> </OrganizerLayout>} />
         <Route path={organizer_url+'event/add-event'} element={<OrganizerLayout> <EventType title={'Select event type'} /> </OrganizerLayout>} />
         <Route path={organizer_url+'event/add-event-basic-info'} element={<OrganizerLayout> <EventBasicInfo title={'Basic Info'} /> </OrganizerLayout>} />
         <Route path={organizer_url+'event/add-event-details'} element={<OrganizerLayout> <EventDetails title={'Event Details'} /> </OrganizerLayout>} />        
