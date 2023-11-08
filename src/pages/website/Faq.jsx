96e4a9d0328de7a0fb853ce8ad8a6248f323079f
unknown <amittechartist@gmail.com>
2023-11-08 16:48:02 +0530

    8

--- src/pages/website/Faq.jsx
+++ src/pages/website/Faq.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem,
+  } from 'reactstrap';
+  
 const Page = ({ title }) => {
     return (
         <>
